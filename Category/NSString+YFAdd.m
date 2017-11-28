091c35735caa852296c179839c72ea46ffcd8c39
yangfei <[yangfei_dev@sina.cn]>
2017-11-28 15:42:48 +0800

    update email validate

--- Category/NSString+YFAdd.m
+++ Category/NSString+YFAdd.m
@@ -74,6 +74,12 @@
     return [predicate evaluateWithObject:mobileNum];
 }
 
+- (BOOL)yf_isValidateEmail:(NSString *)email {
+    NSString *emailRegex = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}";
+    NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", emailRegex];
+    return [emailTest evaluateWithObject:email];
+}
+
 
 + (NSAttributedString *)yf_stringToAttributedText:(NSString *)htmlString {
     NSData *dataStr = [htmlString dataUsingEncoding:NSUnicodeStringEncoding];
